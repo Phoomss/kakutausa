2a1b8deb747686235334b7052d12ba33e648eb7a
Phoomss <narongsak5az@gmail.com>
2025-09-02 13:44:10 +0700

    add router upload

--- backend/src/routes/productRouter.js
+++ backend/src/routes/productRouter.js
@@ -1,18 +1,54 @@
-const productController = require("../controllers/productController");
 const express = require("express");
-const adminMiddleware = require("../middlewares/adminMiddleware");
+const productController = require("../controllers/productController");
 const authMiddleware = require("../middlewares/authMiddleware");
-const e = require("express");
+const adminMiddleware = require("../middlewares/adminMiddleware");
+const upload = require("../utils/uploadStorage");
 
 const productRouter = express.Router();
 
-productRouter.post("/", authMiddleware, adminMiddleware, productController.createProduct);
+productRouter.post(
+    "/",
+    [authMiddleware,
+        adminMiddleware
+    ],
+    productController.createProduct
+);
 
 productRouter.get("/", productController.getAllProducts);
 productRouter.get("/:id", productController.getProductById);
 
-productRouter.put("/:id", authMiddleware, adminMiddleware, productController.updateProduct);
+productRouter.put(
+    "/:id",
+    [authMiddleware,
+        adminMiddleware
+    ],
+    productController.updateProduct
+);
 
-productRouter.delete("/:id", authMiddleware, adminMiddleware, productController.deleteProduct);
+productRouter.delete(
+    "/:id",
+    [authMiddleware,
+        adminMiddleware
+    ],
+    productController.deleteProduct
+);
+
+productRouter.post(
+    "/:id/images",
+    [authMiddleware,
+        adminMiddleware
+    ],
+    upload.single("image"),
+    productController.uploadImage
+);
+
+productRouter.post(
+    "/:id/models",
+    [authMiddleware,
+        adminMiddleware
+    ],
+    upload.fields([{ name: "gltf" }, { name: "bin" }]),
+    productController.uploadModel
+);
 
 module.exports = productRouter;
