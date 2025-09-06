18209765cef4d4fb96ea05d19623a2a00f35c268
Narongsak Pumpasert <101447945+Phoomss@users.noreply.github.com>
2025-09-07 00:22:02 +0700

    Update http-common.js

--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://kakutausa.onrender.com",
   withCredentials: true, // ✅ sent cookies auto
   headers: {
     "Content-Type": "application/json",
