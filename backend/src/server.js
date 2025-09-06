136823bf8351bd34b942d8ed3866068aea4965ac
Phoomss <narongsak5az@gmail.com>
2025-09-07 00:36:36 +0700

    change origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://kakutausa.vercel.app/", 
+    origin: "https://kakutausa.vercel.app", 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
