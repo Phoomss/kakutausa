aaf66f5b4f946332c3d1820c7428d4e31c0c9b99
Phoomss <narongsak5az@gmail.com>
2025-09-18 23:24:17 +0700

    change product service

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -7,34 +7,34 @@ const searchProductByCategory = (category) => {
 };
 
 const getAllProducts = () => http.get(API_URL);
-
 const getProductById = (id) => http.get(`${API_URL}/${id}`);
-
 const createProduct = (data) => http.post(API_URL, data);
-
 const updateProduct = (id, data) => http.put(`${API_URL}/${id}`, data);
-
 const deleteProduct = (id) => http.delete(`${API_URL}/${id}`);
 
-const uploadProductImage = (id, file) => {
+const uploadProductImages = (id, files) => {
   const formData = new FormData();
-  formData.append("images", file);
+  files.forEach((file) => {
+    formData.append("images", file); 
+  });
+
   return http.post(`${API_URL}/${id}/images`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 };
 
-const uploadProductModel = (id, gltfFile, binFile) => {
+const uploadProductModel = (id, gltf, bin, step) => {
   const formData = new FormData();
-  if (gltfFile) formData.append("gltf", gltfFile);
-  if (binFile) formData.append("bin", binFile);
+  if (gltf) formData.append("gltf", gltf);
+  if (bin) formData.append("bin", bin);
+  if (step) formData.append("step", step);
+
   return http.post(`${API_URL}/${id}/models`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
 };
 
 const getProductImages = (id) => http.get(`${API_URL}/${id}/images`);
-
 const getProductModels = (id) => http.get(`${API_URL}/${id}/models`);
 
 const productService = {
@@ -44,7 +44,7 @@ const productService = {
   createProduct,
   updateProduct,
   deleteProduct,
-  uploadProductImage,
+  uploadProductImages,
   uploadProductModel,
   getProductImages,
   getProductModels,
