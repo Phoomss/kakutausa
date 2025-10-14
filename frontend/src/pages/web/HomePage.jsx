408db992b898277b29faf9c8fa0b3c848186395e
Phoomss <narongsak5az@gmail.com>
2025-10-14 21:16:21 +0700

    Remove unused ProductList import from HomePage component

--- frontend/src/pages/web/HomePage.jsx
+++ frontend/src/pages/web/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../../components/web/Hero'
 import AboutSection from '../../components/web/About'
 import ProductCategories from '../../components/web/ProductCategories'
-import ProductList from '../../components/web/ProductList'
 import ProductsByCategory from '../../components/web/ProductsByCategory'
 
 const HomePage = () => {
